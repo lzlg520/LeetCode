21e604d27567a663352e0f24492bd2785adbd9a4
lzlg520 <lzlg520520@126.com>
2019-11-07 22:03:05 +0800

    合并两个有序链表。lzlg

---
+++ src/main/java/com/lzlg/simple/q7/MergeTwoList.java
@@ -0,0 +1,60 @@
+package com.lzlg.simple.q7;
+
+import com.lzlg.ListNode;
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class MergeTwoList {
+    /**
+     * 循环
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode head = new ListNode(-1);
+        ListNode pre = head;
+        while (l1 != null && l2 != null) {
+            if (l1.value <= l2.value) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 == null ? l2 : l1;
+        return head.next;
+    }
+
+    /**
+     * 递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.value < l2.value) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+    }
+}
