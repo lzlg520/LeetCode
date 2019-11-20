6d59f5ff9da86936ea4d4c1518cd2071a0b010b6
lzlg520 <lzlg520520@126.com>
2019-11-20 21:23:54 +0800

    删除元素。lzlg

---
+++ src/main/java/com/lzlg/simple/q9/RemoveElementTest.java
@@ -0,0 +1,56 @@
+package com.lzlg.simple.q9;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 nums = [3,2,2,3], val = 3,
+ * <p>
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例 2:
+ * <p>
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ * <p>
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ * <p>
+ * 注意这五个元素可为任意顺序。
+ * <p>
+ * 你不需要考虑数组中超出新长度后面的元素。
+ */
+public class RemoveElementTest {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null) return 0;
+        int len = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                len++;
+                j++;
+            }
+        }
+        return len;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int c = removeElement(nums, 2);
+        System.out.println(c);
+        System.out.println("=================");
+        System.out.println(Arrays.toString(nums));
+    }
+}
