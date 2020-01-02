40e9857457365837c3d535f2908174ff93852c20
lzlg520 <lzlg520520@126.com>
2020-01-02 12:57:08 +0800

    git ignore

--- src/main/java/com/lzlg/simple/Template.java
+++ src/main/java/com/lzlg/simple/Template.java
@@ -6,10 +6,16 @@ import org.junit.Test;
  *
  */
 public class Template {
+    static {
+    }
+    public static int a = 10;
 
 
     @Test
     public void test() {
 
+    }
+
+    interface Function {
     }
 }
